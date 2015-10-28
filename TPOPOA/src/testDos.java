462fcd992400135556ba7f7c4ed8bf8801ec68c5
Yoni <Yoni@YONI-PC>
2015-10-28 12:20:17 -0300

    Agregamos hilos, creamos otro test, intentamos sincronizar los hilos clientes para obtener alguno de los dos cajeros, pero no anda.

--- TPOPOA/src/testDos.java
+++ TPOPOA/src/testDos.java
@@ -17,8 +17,8 @@ public class testDos {
     /**
     *yoni:- mejorar constructores Banco, cliente y Cuenta*/
     public static void main(String[] args) {
-        System.out.println("de prueba para GIT");
-        System.out.println("asdfg56g165");
+        System.out.println("PRueba para push");
+        System.out.println("YONI ");
 //        System.out.println("");
         Cliente a = new Cliente("Yoni", "1", "aca cerca");
         Cliente b = new Cliente("sadg", "2", "aca cerca");
